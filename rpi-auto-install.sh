7627c32de0cf14ce3e2bf054aa14380f5a7bd40f
ilhanakilli <ilhanakilli@gmail.com>
2017-01-29 16:40:27 +0200

    Create rpi-auto-install.sh

---
+++ rpi-auto-install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get --ignore-missing install -y filezilla audacity geany inkscape audacious unetbootin stellarium mps-youtube dropbox shutter soundconverter gparted furiusisomount deluge p7zip pinta htop sublime pdfshuffler hardinfo samba system-config-samba

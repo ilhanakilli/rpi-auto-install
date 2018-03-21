8efdb9aa0ec422772a7ddc844965a5ca89e13b8c
ilhanakilli <ilhanakilli@gmail.com>
2018-03-21 15:18:51 +0300

    Update linuxdesktop.sh

--- linuxdesktop.sh
+++ linuxdesktop.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 sudo apt --ignore-missing install -y furiusisomount unetbootin devede smplayer leafpad gedit vlc k3b cheese openshot brasero kodi geany eclipse dropbox kdenlive godot snes9x filezilla audacity inkscape audacious stellarium shutter soundconverter gparted deluge p7zip pinta htop pdfshuffler hardinfo gimp xpad guake
